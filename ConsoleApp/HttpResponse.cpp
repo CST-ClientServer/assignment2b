9b06f3bc7309903884e0d1f1fcdedd107783187e
Jonathan <122506441+jwl0724@users.noreply.github.com>
2024-11-13 05:27:09 -0800

    implement class

--- ConsoleApp/HttpResponse.cpp
+++ ConsoleApp/HttpResponse.cpp
@@ -1,1 +1,16 @@
 #include "HttpResponse.hpp"
+
+#include <sstream>
+
+HttpResponse::HttpResponse(string response) : response(std::move(response)) {
+    stringstream stream{response};
+    string buffer;
+    stream >> buffer >> statusCode;
+}
+
+string HttpResponse::extractBody() {
+    string bodyStartIdentifier = separator + separator;
+    size_t bodyStart = response.find(bodyStartIdentifier);
+    string body = response.substr(bodyStart + bodyStartIdentifier.size());
+    return body;
+}
