a6459d5ffafa072ef3c06787d85fb2fdc5dca9f4
Jonathan <122506441+jwl0724@users.noreply.github.com>
2024-11-13 05:27:20 -0800

    implement class

--- ConsoleApp/HttpRequest.cpp
+++ ConsoleApp/HttpRequest.cpp
@@ -1,1 +1,42 @@
 #include "HttpRequest.hpp"
+
+void HttpRequest::addFile(vector<byte> fileBytes, string name) {
+    string fileSection;
+    fileSection.append("--").append(boundary).append(separator);
+    fileSection.append(contentDisposition).append("form-data; name=\"File\"; filename=\"").append(name).append("\"").append(separator);
+    fileSection.append("Content-Type: application/octet-stream").append(separator).append(separator);
+    string fileAsStringBytes{reinterpret_cast<const char*>(&fileBytes[0], fileBytes.size())};
+    fileSection.append(fileAsStringBytes);
+    requestComponents.push_back(fileSection);
+}
+
+void HttpRequest::addText(string text, string name) {
+    string textSection;
+    textSection.append("--").append(boundary).append(separator);
+    textSection.append(contentDisposition).append("form-data; name=\"").append(name).append("\"").append(separator);
+    textSection.append("Content-Type: text/plain").append(separator);
+    textSection.append(separator).append(text).append(separator);
+    requestComponents.push_back(textSection);
+}
+
+string HttpRequest::buildHeader(string host, string endpoint, int port) const {
+    string header = "POST ";
+    header.append(endpoint).append(" HTTP/1.1").append(separator);
+    header.append("Host: ").append(host).append(":").append(to_string(port)).append(separator);
+    header.append("Content-Type: ").append(contentType).append("; boundary=").append(boundary).append(separator);
+    header.append("Connection: close");
+
+    // calculate content size
+    long totalBytes = header.size();
+    for (string component : requestComponents) totalBytes += component.size();
+
+    // set header to content size
+    header.append("Content-Length: ").append(to_string(totalBytes)).append(separator);
+    return header;
+}
+
+string HttpRequest::getRequestString(string host, string endpoint, int port) const {
+    string httpRequest = buildHeader(host, endpoint, port);
+    for(string component : requestComponents) httpRequest.append(component);
+    return httpRequest;
+}
--- ConsoleApp/HttpRequest.hpp
+++ ConsoleApp/HttpRequest.hpp
@@ -1,8 +1,25 @@
 #ifndef ASSIGNMENT2B_HTTPREQUEST_HPP
 #define ASSIGNMENT2B_HTTPREQUEST_HPP
 
-class HttpRequest {
+#include <utility>
+#include <vector>
+#include <string>
 
+using namespace std;
+
+class HttpRequest {
+    const string separator = "\r\n";
+    const string boundary = "-------MyCustomBounds69420";
+    const string contentDisposition = "Content-Disposition: ";
+
+    vector<string> requestComponents;
+    string contentType;
+    string buildHeader(string host, string endpoint, int port) const;
+public:
+    explicit HttpRequest(string contentType = "multipart/form-data") : contentType(std::move(contentType)){};
+    void addFile(vector<byte> fileBytes, string name);
+    void addText(string text, string name);
+    string getRequestString(string host, string endpoint, int port) const;
 };
 
 #endif //ASSIGNMENT2B_HTTPREQUEST_HPP
