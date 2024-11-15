1bc4b983e881daa08aa0f80d690610f4330eb765
Juntingg <jsu62@my.bcit.ca>
2024-11-14 23:46:03 -0800

    add readRequest function on HttpServletRequest

--- UploadServer/HttpServletRequest.hpp
+++ UploadServer/HttpServletRequest.hpp
@@ -3,11 +3,15 @@
 
 #include <sys/types.h>
 #include <sys/unistd.h>
+#include <sstream>
+#include <iostream>
 
 class HttpServletRequest {
 public:
     // constructor
-    explicit HttpServletRequest(int clientSocket) : clientSocket(clientSocket) {}
+    explicit HttpServletRequest(int clientSocket) : clientSocket(clientSocket) {
+        readRequest();
+    }
 
     // reads data from the client socket into a provided buffer.
     // :param buffer: pointer to a char array where data is stored.
@@ -25,8 +29,36 @@ public:
         }
     }
 
+    std::string getMethod() {
+        return method;
+    }
+
+    std::string getPath() {
+        return path;
+    }
+
 private:
     int clientSocket;  // socket file descriptor for the client connection
+    std::string method;
+    std::string path;
+
+    void readRequest() {
+        char buffer[1024];
+        ssize_t bytesRead = ::read(clientSocket, buffer, sizeof(buffer) - 1);
+
+        if (bytesRead <= 0) {
+            std::cerr << "Error reading request." << std::endl;
+            return;
+        }
+
+        buffer[bytesRead] = '\0';
+
+        std::string request(buffer);
+        std::istringstream requestStream(request);
+
+        requestStream >> method >> path;
+    }
 };
+
 
 #endif
